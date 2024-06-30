36db0a76113e59c585a4c6e794da6fe0ca80dc84
marceloacunaosorio <m.acunao@duocuc.cl>
2024-06-29 23:32:42 -0400

    se agrega *ngFor

--- AppWebJson001/src/app/componets/aventura/aventura.component.ts
+++ AppWebJson001/src/app/componets/aventura/aventura.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component,OnInit} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { JsonService } from '../../services/json.service';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-aventura',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule,CommonModule,FormsModule],
   templateUrl: './aventura.component.html',
-  styleUrl: './aventura.component.css'
+  styleUrl: './aventura.component.css',
+  providers: [JsonService]
 })
-export class AventuraComponent {
+export class AventuraComponent  implements OnInit{
+   productos: any[]=[];
+   nombre: string = '';
+   descripcion:string = '';
+   precio: number | null = null;
 
+   constructor(private jsonService: JsonService){}
+
+   ngOnInit(): void {
+       this.jsonService.getJsonData().subscribe(data =>{
+        this.productos = data;
+       });
+   }
 }
